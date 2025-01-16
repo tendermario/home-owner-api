fe3dbb57b0b06ddcb69d29a50624b07793f5441e
Mitch Burton <mitch@mitchellburton.ca>
2025-01-15 18:51:36 -0800

    add api to allowed hosts; attempt static url fix

--- homeowner/settings.py
+++ homeowner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)xc4b8d*#1z=r3ua73si#o_e2$1rv&qkhz43da$*oyc%pf@(@p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'api/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
